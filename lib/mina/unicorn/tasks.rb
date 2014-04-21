a6eff05dca72c82e10b59b591facfe5c72f64732
Andrew Volozhanin <linuxheadrus@gmail.com>
2014-04-21 17:02:24 +0600

    Refactor using capistrano-unicorn as an example

--- lib/mina/unicorn/tasks.rb
+++ lib/mina/unicorn/tasks.rb
@@ -1,35 +1,35 @@
 require 'mina/bundler'
 require 'mina/rails'
+require 'mina/unicorn/utility'
 
 namespace :unicorn do
+  include Mina::Unicorn::Utility
+
   set_default :unicorn_role,      -> { user }
   set_default :unicorn_env,       -> { fetch(:rails_env, 'production') }
   set_default :unicorn_config,    -> { "#{deploy_to}/#{current_path}/config/unicorn.rb" }
   set_default :unicorn_pid,       -> { "#{deploy_to}/#{current_path}/tmp/pids/unicorn.pid"  }
   set_default :unicorn_cmd,       -> { "#{bundle_prefix} unicorn" }
+  set_default :unicorn_restart_sleep_time, -> { 2 }
 
-  desc "Start unicorn service"
-  task :start => :environment do
-    queue %{
-      echo "-----> Starting unicorn service"
-      #{echo_cmd %[cd #{deploy_to}/#{current_path}; #{unicorn_cmd} -c #{unicorn_config} -D -E #{unicorn_env}]}
-    }
+  desc "Start Unicorn master process"
+  task start: :environment do
+    queue start_unicorn
   end
 
-  desc "Stop unicorn service"
-  task :stop => :environment do
-    queue %{
-      echo "-----> Stoping unicorn service"
-      #{echo_cmd %[kill -s QUIT `cat #{unicorn_pid}`]}
-    }
+  desc "Stop Unicorn"
+  task stop: :environment do
+    queue kill_unicorn('QUIT')
+  end
+
+  desc "Immediately shutdown Unicorn"
+  task shutdown: :environment do
+    queue kill_unicorn('TERM')
   end
 
   desc "Restart unicorn service"
-  task :restart => :environment do
-    queue %{
-      echo "-----> Restart unicorn service"
-      #{echo_cmd %[kill -s USR2 `cat #{unicorn_pid}`]}
-    }
+  task restart: :environment do
+    queue restart_unicorn
   end
 end
 
