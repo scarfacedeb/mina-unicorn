39fe82e10a0d5763750c948148bf0bc501829791
Cassidy K <hello@cassidy.codes>
2018-03-20 07:49:02 -0400

    Fix mina deprecation warnings

--- lib/mina/unicorn/tasks.rb
+++ lib/mina/unicorn/tasks.rb
@@ -13,17 +13,17 @@ namespace :unicorn do
   set :bundle_gemfile,    -> { "#{fetch(:current_path)}/Gemfile" }
 
   desc "Start Unicorn master process"
-  task start: :remote_environment do
+  task :start do
     command start_unicorn
   end
 
   desc "Stop Unicorn"
-  task stop: :remote_environment do
+  task :stop do
     command kill_unicorn("QUIT")
   end
 
   desc "Immediately shutdown Unicorn"
-  task shutdown: :remote_environment do
+  task :shutdown do
     command kill_unicorn("TERM")
   end
 
